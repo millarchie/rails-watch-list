f366a4f8c54175cd24bcda691a358bcd157b4735
millarchie <archie.millar@hotmail.com>
2026-05-14 15:25:56 +0900

    lists#new lists#create done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,10 +9,22 @@ class ListsController < ApplicationController
   end
 
   def new
+    # empty new instance to build the form:
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
