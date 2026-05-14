6639667ff46e2c95b7dc093a3115d9cb5e2b2280
millarchie <archie.millar@hotmail.com>
2026-05-14 15:14:10 +0900

    lists#index and show done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+end
